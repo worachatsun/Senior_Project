99f6a08262e42441fb47a034cc31d791b2a5c345
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-03-15 23:11:42 +0700

    edit Bigcard

--- src/common/BigCard.js
+++ src/common/BigCard.js
@@ -1,30 +1,44 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 const BigCard = (props) => {
+    const { containerStyle, viewStyle, textStyle } = styles
     return (
-        <View style={styles.containerStyle}>
-            {props.children}
+        <View style={containerStyle}>
+            <View>
+                {props.children}
+            </View>
+            <View style={viewStyle}>
+            <Text style={textStyle} numberOfLines={2}>
+                คณะเทคโนโลยีสารสนเทศ (SIT) ร่วมกับบริษัทอินเตอรฺ์ลิ้งค์ คอมมิวนิเคชั่น จำกัด (มหาชน) ผู้นำธุรกิจสายสัญญาณคอมพิวเตอร์และสื่อสารโทรคมนาคม  ได้จัดกิจกรรม Link Campus Cabling 2017 ในหัวข้อ "Open Cabling System for the Future" ให้กับนักศึกษา SIT 
+            </Text>
+        </View>
         </View>
     )
 }
 
 const styles = {
     containerStyle: {
-        borderWidth: 1,
-        borderRadius: 2,
-        borderColor: '#ddd',
         borderBottomWidth: 0,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 2,
         elevation: 1,
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 12,
         height: 300,
         backgroundColor: '#FEFEFF'
+    },
+    viewStyle: {
+        padding: 15,
+        height: 100,
+        flex: 1,
+        justifyContent: 'center',
+    },
+    textStyle: {
+        fontSize: 12
     }
 }
 
--- src/components/NewsPage.js
+++ src/components/NewsPage.js
@@ -13,7 +13,8 @@ import {
     CardSection, 
     Content,
     SlideupContainer,
-    BottomNavBar
+    BottomNavBar,
+    Card
 } from '../common'
 
 class NewsPage extends Component {
@@ -24,10 +25,12 @@ class NewsPage extends Component {
                 <ScrollView>
                     <BigCard>
                         <Image source={{uri: 'https://www4.sit.kmutt.ac.th/files/story_pictures/IMG_5299.JPG#overlay-context=content/sit-%25E0%25B8%2588%25E0%25B8%25B1%25E0%25B8%259A%25E0%25B8%25A3%25E0%25B9%2588%25E0%25B8%25A7%25E0%25B8%25A1%25E0%25B8%25A1%25E0%25B8%25B7%25E0%25B8%25AD%25E0%25B8%2581%25E0%25B8%25B1%25E0%25B8%259A-interlink-%25E0%25B8%2588%25E0%25B8%25B1%25E0%25B8%2594-link-campus-cabling-2017'}}
-        style={{resizeMode: 'contain',width: null,height: 210}} />
-                        <CardSection />
-                        <Content />
+        style={{resizeMode: 'stretch', width: null, height: 230}} />
                     </BigCard>
+                    <Card>
+                        <Image source={{uri: 'https://www4.sit.kmutt.ac.th/files/story_pictures/IMG_5299.JPG#overlay-context=content/sit-%25E0%25B8%2588%25E0%25B8%25B1%25E0%25B8%259A%25E0%25B8%25A3%25E0%25B9%2588%25E0%25B8%25A7%25E0%25B8%25A1%25E0%25B8%25A1%25E0%25B8%25B7%25E0%25B8%25AD%25E0%25B8%2581%25E0%25B8%25B1%25E0%25B8%259A-interlink-%25E0%25B8%2588%25E0%25B8%25B1%25E0%25B8%2594-link-campus-cabling-2017'}}
+        style={{resizeMode: 'stretch', width: null, height: 230}} />
+                    </Card>
                 </ScrollView>
                 <BottomNavBar />    
             </View>
