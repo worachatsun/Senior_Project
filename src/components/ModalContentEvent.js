0f24fc0979efe569763852b596fcac70caa46e9e
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-03-21 00:29:15 +0700

    event modal on

--- src/components/ModalContentEvent.js
+++ src/components/ModalContentEvent.js
@@ -1,46 +1,18 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, ListView, ScrollView } from 'react-native'
-import { Card, ImageModal, CardSection, ModalHeader } from '../common'
+import { View, Text } from 'react-native'
+import ModalHeader from '../common/ModalHeader'
 
 class ModalContentEvent extends Component {
-
     render () {
-        const { headerTextStyle, contentTextStyle, viewStyle } = styles
-
         return (
-            <View style={{flex: 1}}>
+            <View>
                 <View>
-                    <ModalHeader headerText={'TITLE'} />
+                    <ModalHeader headerText={'TEXT'} />
                 </View>
-                <ScrollView>
-                    <ImageModal img={'https://www.w3schools.com/css/img_forest.jpg'} />
-                    <View style={{borderBottomWidth: 1, borderColor: '#ddd',}}>
-                        <Text style={headerTextStyle}>TITLE</Text>
-                    </View>
-                    <View style={viewStyle}>
-                        <Text style={contentTextStyle}>TITLE</Text>
-                    </View>
-                </ScrollView>
+                <Text>asdasd</Text>
             </View>
         )
     }
 }
 
-const styles = {
-    headerTextStyle: {
-        margin: 20,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        fontSize: 25
-    },
-    contentTextStyle: {
-        margin: 20,
-        textAlign: 'center'
-    },
-    viewStyle: {
-        alignItems: 'center'
-    }
-}
-
 export default ModalContentEvent
-
