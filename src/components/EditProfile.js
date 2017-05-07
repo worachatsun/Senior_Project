f75b74dc61e04e747339de47028872693c5d71e8
worachatsun <worachat.sun@mail.kmutt.ac.th>
2017-05-07 15:55:30 +0700

    change profile ui

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity, Dimensions, Image, ScrollView, TextInput } from 'react-native'
 import { Actions } from 'react-native-router-flux'
-import { ModalHeaderPlain, EmptyCard, RoundImage } from '../common'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+import { ModalHeaderPlain, EmptyCard, RoundImage, CardSection } from '../common'
 import NewsFavorite from './NewsFavorite'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 class EditProfile extends Component {
 
@@ -31,54 +34,74 @@ class EditProfile extends Component {
     // }
 
     render () {
+        const { assets, name, email, surname } = this.props.profile.user_detail.user
+
         return (
-            <View style={{flex: 1, backgroundColor: '#ddd'}}>
+            <View style={{flex: 1}}>
                 <ModalHeaderPlain headerText={'Profile'}/>
                 <ScrollView>
                     <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 18}}>
-                        <RoundImage img={"https://www4.sit.kmutt.ac.th/files/story_pictures/IMG_0027.jpg"} />
-                        <Text style={{ margin: 12, fontSize: 18}}>Sukrit Wisetkaeo</Text>
+                        <RoundImage img={assets.picture} />
+                        <Text style={{ margin: 12, fontSize: 18}}>{name} {surname}</Text>
                     </View>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/tel.png')} /> 
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(tel) => this.setState({tel})} value={this.state.tel} />
-                            </View>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/email.png')} /> 
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(email) => this.setState({email})} value={this.state.email} />
-                            </View>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/location.png')} /> 
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(location) => this.setState({location})} value={this.state.location} />
-                            </View>
+                    <CardSection />
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"information"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Info</Text>
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/hat.png')} /> 
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(faculty) => this.setState({faculty})} value={this.state.faculty} />
-                            </View>
+                        
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/tel.png')} /> 
+                            <TextInput value={this.state.tel} onChangeText={(tel) => this.setState({tel})}/>
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/career.png')} /> 
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(job) => this.setState({job})} value={this.state.job} />
-                            </View>
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/email.png')} /> 
+                            <TextInput value={email} />
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/trophy.png')} />
-                                <TextInput style={{borderColor: 'gray', borderWidth: 1, width: 200}} onChangeText={(trophy) => this.setState({trophy})} value={this.state.trophy} />
-                            </View>
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/location.png')} /> 
+                            <TextInput value={this.state.location} onChangeText={(location) => this.setState({location})}/>
                         </View>
-                    </EmptyCard>
+                    </View>
+                
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"school"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Graduated</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/hat.png')} /> 
+                            <TextInput value={this.state.faculty} onChangeText={(faculty) => this.setState({faculty})}/>
+                        </View>
+                    </View>
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"worker"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Career</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/career.png')} /> 
+                            <TextInput value={this.state.job} onChangeText={(job) => this.setState({job})}/>
+                        </View>
+                    </View>
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"trophy"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Trophy</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/trophy.png')} /> 
+                            <TextInput value={this.state.trophy} onChangeText={(trophy) => this.setState({trophy})}/>
+                        </View>
+                    </View>
                 </ScrollView>
             </View>
         )
@@ -98,4 +121,8 @@ const styles = {
 
 const {height, width} = Dimensions.get('window')
 
-export default EditProfile
+const mapStateToProps = state => {
+    return { profile: state.auth }
+}
+
+export default connect(mapStateToProps, actions)(EditProfile)
--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -3,8 +3,9 @@ import { connect } from 'react-redux'
 import { View, Text, TouchableOpacity, Dimensions, Image, ScrollView } from 'react-native'
 import * as actions from '../actions'
 import { Actions } from 'react-native-router-flux'
-import { Header, EmptyCard, RoundImage } from '../common'
+import { Header, EmptyCard, RoundImage, CardSection } from '../common'
 import NewsFavorite from './NewsFavorite'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 class ProfilePage extends Component {
 
@@ -17,65 +18,84 @@ class ProfilePage extends Component {
         const { assets, name, email, surname } = this.props.profile.user_detail.user
 
         return (
-            <View style={{flex: 1, marginBottom: 50}}>
+            <View style={{flex: 1}}>
                 <Header headerText={'Profile'} rightIcon={"edit"} leftIcon={'back'}/>
                 <ScrollView>
                     <View style={{justifyContent: 'center', alignItems: 'center', marginTop: 18}}>
                         <RoundImage img={assets.picture} />
                         <Text style={{ margin: 12, fontSize: 18}}>{name} {surname}</Text>
                     </View>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/tel.png')} /> 
-                                <Text>
-                                    080-000-0000
-                                </Text>
-                            </View>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10, justifyContent: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/email.png')} /> 
-                                <Text>
-                                    {email}
-                                </Text>
-                            </View>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 , justifyContent: 'center'}}>
-                                <Image style={styles.iconStyle} source={require('../env/images/location.png')} /> 
-                                <Text>
-                                    เชียงใหม่ ประเทศไทย
-                                </Text>
-                            </View>
+                    <CardSection />
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"information"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Info</Text>
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/hat.png')} /> 
-                                <Text>
-                                    KMUTT
-                                </Text>
-                            </View>
+                        
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/tel.png')} /> 
+                            <Text>
+                                080-000-0000
+                            </Text>
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/career.png')} /> 
-                                <Text>
-                                    นักร้อง
-                                </Text>
-                            </View>
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/email.png')} /> 
+                            <Text>
+                                {email}
+                            </Text>
                         </View>
-                    </EmptyCard>
-                    <EmptyCard>
-                        <View style={{margin: 14}}>
-                            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                                <Image style={styles.iconStyle} source={require('../env/images/trophy.png')} /> 
-                                <Text>
-                                    ผลงานศิสเก่า
-                                </Text>
-                            </View>
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/location.png')} /> 
+                            <Text>
+                                เชียงใหม่ ประเทศไทย
+                            </Text>
                         </View>
-                    </EmptyCard>
+                    </View>
+                
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"school"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Graduated</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/hat.png')} /> 
+                            <Text>
+                                KMUTT
+                            </Text>
+                        </View>
+                    </View>
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"worker"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Career</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/career.png')} /> 
+                            <Text>
+                                นักร้อง
+                            </Text>
+                        </View>
+                    </View>
+                
+                    <View style={{margin: 14}}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Icon style={{color: "#ff7f11"}} name={"trophy"} size={20}/>
+                            <Text style={{marginLeft: 3, color: "#ff7f11"}}>Trophy</Text>
+                        </View>
+                        <CardSection />
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                            <Image style={styles.iconStyle} source={require('../env/images/trophy.png')} /> 
+                            <Text>
+                                ผลงานศิสเก่า
+                            </Text>
+                        </View>
+                    </View>
+                    
                 </ScrollView>
             </View>
         )
